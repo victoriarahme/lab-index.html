dd3606b0cb1eb2e9b276c8c0ea6fe768a0f8d56a
victoriarahme <vic.rahme@gmail.com>
2024-09-20 19:01:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+
+ //declaring using string//
+ let studentName = "Victoria Rahme";
+ console.log(studentName);
+
+//declaring using a number//
+ let studentAge = "26";
+ console.log(studentAge);
+
+//declaring using a boolean//
+ let isEnrolled = true;
+ console.log(isEnrolled);
+
+ //declaring array//
+
+ let shoes = ['converse', 'nike', 'adidas']
+ console.log(shoes);
+
+
+//declaring object//
+let dog = {
+	breed: "poodle",
+	color: "black",
+
+};
+
+console.log(dog.breed);
+
+
+//arithmetic operators//
+console.log(10*10);
+
+console.log(100 / 2);
+
+console.log(15 % 5);
+
+console.log(100 - 25);
+
+console.log(300 + 10);
+
+console.log(300 - 10);
+
+
+//comparison operators//
+
+let x = 3;
+let y = 30;
+console.log("30" == y); //equal//
+console.log(x != y); //inequal//
+console.log(x > y); //greater//
+console.log(x < y); //less//
+console.log(x <= y); //less or equal//
+console.log(x >= y); //greater or equal//
+
+//logical operators//
+
+let a = true;
+let b = false;
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+console.log(!a); // Logical NOT
+
+
+
+
+
+
+
+
+
+
